cdc982f227c130ce11e6ef2ced459835d223e52b
jess alejo <jess@ubuntu.(none)>
2014-01-29 18:24:17 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweetZombie::Application.config.secret_key_base = 'd4ce779dc3b8a117098c70863e723f22c446d765f0b54a17ececac58476fbbcdff97495869be41a3ca337f5709887dd223ee48f2ead3661b2f403bd62f978481'
